57bc99281e25d8c1887d407dd86cf7e9e4d2bf41
meetkothari-crest <meet.k@crestskillserve.com>
2025-03-05 13:46:35 +0530

    Done some minor modification

--- main.py
+++ main.py
@@ -2,45 +2,42 @@
 import json
 import boto3
 
-def getResponse():
 # Set up the Amazon Bedrock client
-    bedrock_client = boto3.client(
-        	service_name="bedrock-runtime",
-            region_name="us-east-1"
-    )
+bedrock_client = boto3.client(
+        service_name="bedrock-runtime",
+        region_name="us-east-1"
+)
 # Define the model ID
-    model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
+model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
+
 # Prepare the input prompt.
-    prompt = "Give AWS AI Practitioner Syllabus"
+prompt = "Hello, How are you?"
 
-    payload = {
-        "anthropic_version": "bedrock-2023-05-31",
-        "max_tokens": 200,
-        "temperature": 0.9,
-        "top_k": 250,
-        "top_p": 1,
-        "messages": [
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": prompt
-                    }
-                ]
-            }
-        ]
-    }
-    # Invoke the Amazon Bedrock model
-    response = bedrock_client.invoke_model(
-        modelId=model_id,
-        body=json.dumps(payload)
-    )
+payload = {
+    "anthropic_version": "bedrock-2023-05-31",
+    "max_tokens": 200,
+    "temperature": 0.9,
+    "top_k": 250,
+    "top_p": 1,
+    "messages": [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": prompt
+                }
+            ]
+        }
+    ]
+}
+# Invoke the Amazon Bedrock model
+response = bedrock_client.invoke_model(
+    modelId=model_id,
+    body=json.dumps(payload)
+)
 
-    # Process the response
-    result = json.loads(response["body"].read())
-    generated_text = "".join([output["text"] for output in result["content"]])
-    print(f"Response: {generated_text}")
-
-if __name__ == "__main__":
-    getResponse()
+# Process the response
+result = json.loads(response["body"].read())
+generated_text = "".join([output["text"] for output in result["content"]])
+print(f"Response: {generated_text}")
